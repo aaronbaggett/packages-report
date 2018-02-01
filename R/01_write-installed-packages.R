be877fd1906c26ec3d04f886ebca7c85a25e150b
Aaron Baggett <aaronbaggett@gmail.com>
2018-02-01 11:54:17 -0800

    Initial workshop commit

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,10 @@
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
+library(tidyverse)
+pckgs <- installed.packages() %>%
+  as_tibble() %>%
+  select(Package, LibPath, Version, Priority, Built)
 
 ## keep the variables
 ##   * Package
@@ -13,6 +17,15 @@
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+library(fs)
+(from_dir <- file.path("~", "Desktop", "packages-report", "data"))
+(from_files <- dir_ls(from_dir, glob = "*.csv"))
+(to_files <- path_file(from_files))
+(out <- file_copy(from_files, to_files))
+dir_ls()
+dir_info()
+
+readr::write_csv(pckgs, path = "~/Desktop/packages-report/data/add-on-packages.csv")
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
