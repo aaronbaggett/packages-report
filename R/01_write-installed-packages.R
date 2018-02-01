2a6066664a353228c01092fc0f4a5b1e3a48ca6a
Aaron Baggett <aaronbaggett@gmail.com>
2018-02-01 11:55:24 -0800

    Update installed-packages

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -17,14 +17,6 @@ pckgs <- installed.packages() %>%
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
-library(fs)
-(from_dir <- file.path("~", "Desktop", "packages-report", "data"))
-(from_files <- dir_ls(from_dir, glob = "*.csv"))
-(to_files <- path_file(from_files))
-(out <- file_copy(from_files, to_files))
-dir_ls()
-dir_info()
-
 readr::write_csv(pckgs, path = "~/Desktop/packages-report/data/add-on-packages.csv")
 
 ## when this script works, stage & commit it and the csv file
